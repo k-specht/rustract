403c9bd89f2a91281cc6dbfdf05c89b0d383051b
k-specht <xpecht@gmail.com>
2021-09-17 21:52:04 -0400

    Updated error messages for size limits

--- src/types.rs
+++ src/types.rs
@@ -207,9 +207,10 @@ where T: HasLength
     match value.length() > field_design.characters {
         true => Err(BackendError {
             message: format!(
-                "Field {} is over the size limit of {}.",
+                "Field {} is over the size limit of {}.\n(Size: {}).",
                 field_design.title,
-                field_design.characters
+                field_design.characters,
+                value.length()
             ),
         }),
         false => Ok(())
@@ -223,9 +224,10 @@ where T: HasBytes
     match value.byte_length() > field_design.bytes {
         true => Err(BackendError {
             message: format!(
-                "Field {} is over the byte limit of {}.",
+                "Field {} is over the byte limit of {}.\n(Bytes: {}).",
                 field_design.title,
-                field_design.bytes
+                field_design.bytes,
+                value.byte_length()
             ),
         }),
         false => Ok(()),
@@ -437,7 +439,7 @@ mod test {
                 title: String::from("email"),
                 datatype: DataType::String,
                 bytes: 800,
-                characters: 100,
+                characters: 110,
                 decimals: None,
                 regex_bound: true,
                 regex: String::from("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"),
